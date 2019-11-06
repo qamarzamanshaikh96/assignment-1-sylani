26e8feae923dc873e94c613ed2021ad49afe1453
unknown <zamanshaikh3@gmail.com>
2019-11-06 14:08:45 -0800

    assignment

---
+++ Assignment 1 sylani.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[60]:
+
+
+a = ('Twinkle, twinkle, little star')
+z = ("       ")
+x = ("             ")
+y = ("             ")
+b =       ('How i wonder what you are!')
+c =              ('up above the world so high,')
+d =              ('like a diamond in a sky.')
+e = ('Twinkle, twinkle, little star,')
+f = ('How i wonder what you are')
+print(a)
+print(z+b)
+print(x+c)
+print(y+d)
+print(e)
+print(f)
+
+
+# In[78]:
+
+
+import sys
+
+system = "Python version"
+versionInfo = "version information"
+
+print(system, sys.version)
+
+
+# In[71]:
+
+
+from datetime import datetime
+now = datetime.now()
+print("now = ", now)
+dt_string = now.strftime("%d/%m/%y %H%M%S")
+print("date and time =", dt_string)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
